0606b3318b5de330f867b08ce0db755202d47952
Vu Nguyen <nguyen.vu.27022001@gmail.com>
2022-05-07 08:22:14 +0700

    init project

---
+++ src/test/java/productMaintenance/Thuchanh2ApplicationTests.java
@@ -0,0 +1,13 @@
+package productMaintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Thuchanh2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
